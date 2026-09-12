d36338210129477a9af93f5abfa2b1aae5b77d11
47arsh <arxhx7@gmail.com>
2026-09-12 23:25:35 +0530

    refactor user API into routes and controllers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+import User from "../models/User.js";
+
+const createUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const newUser = await User.create({
+            name,
+            email,
+            password
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            user: newUser
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating user"
+        });
+    }
+};
+
+export { createUser };
