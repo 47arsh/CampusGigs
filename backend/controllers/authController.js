8dd89fd1e29f86af51841eaa568bb6c0b55fb07b
47arsh <arxhx7@gmail.com>
2026-09-13 00:35:11 +0530

    add user registration with password hashing

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                message: "User already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            message: "User registered successfully",
+            user: {
+                _id: newUser._id,
+                name: newUser.name,
+                email: newUser.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Error registering user"
+        });
+    }
+};
+
+export { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/api/users",userRoutes);
 app.use("/api/tasks",taskRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
