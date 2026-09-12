884e0d9f742e72ee22605e8bfc1e3cee6b7b994a
47arsh <arxhx7@gmail.com>
2026-09-12 22:16:25 +0530

    backend connected to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get("/" , (req,res) => {
-    res.send({
-        "message" : "welcome to CampusGigs API"
-    })
-})
-
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.get("/", (req, res) => {
+    res.json({
+        message: "Welcome to CampusGigs API"
+    });
 });
+
+const startServer = async () => {
+    await connectDB();
+
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+};
+
+startServer();
